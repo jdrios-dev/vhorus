12a53d9055cf9c64ad81ebd4b9bb28594f1448c0
Juan Daniel Rios Ramirez <mrdaniel19996@gmail.com>
2022-07-16 21:09:36 -0500

    first commit

---
+++ components/Carousel.tsx
@@ -0,0 +1,49 @@
+import Image from "next/image";
+import React, { useState } from "react";
+import { flag } from "../data";
+import styles from "../styles/Home.module.css";
+
+const Carousel = () => {
+  const [activeImg, setActiveImg] = useState(flag[0]);
+
+  return (
+    <div>
+      <h1>vHorus Team</h1>
+      <div className={styles.mainImageCont}>
+        <Image
+          src={activeImg.image}
+          layout="fill"
+          className={styles.mainImage}
+          alt={activeImg.title}
+        />
+      </div>
+
+      <div className={styles.imagesContainer}>
+        {flag.map((item) => (
+          <div
+            key={item.image}
+            className={styles.imagesItem}
+            onClick={() => setActiveImg(item)}
+          >
+            <Image
+              src={item.image}
+              layout="fill"
+              alt={item.title}
+              style={{ borderRadius: "10px" }}
+            />
+          </div>
+        ))}
+      </div>
+
+      <p
+        style={{
+          width: "800px",
+        }}
+      >
+        {activeImg.text}
+      </p>
+    </div>
+  );
+};
+
+export default Carousel;
